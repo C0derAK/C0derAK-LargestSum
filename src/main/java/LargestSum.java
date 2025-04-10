2897107ef9cb0446f3f4ae32f51adb6d5007dfa5
C0derAK <parmar.aditya.in@gmail.com>
2025-04-10 20:18:36 +0530

    Done

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -1,5 +1,7 @@
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LargestSum {
     /**
@@ -10,6 +12,25 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        if (nums == null || nums.size() < 2) return 0;
+        
+        Map<Integer, Integer> freq = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+            if (num > max) {
+                secondMax = max;
+                max = num;
+            } else if (num > secondMax && num != max) {
+                secondMax = num;
+            }
+        }
+        if (freq.get(max) >= 2) {
+            return max + max;
+        } else {
+            return max + secondMax;
+        }
     }
 }
